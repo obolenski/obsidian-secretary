cdc6d14a6e01f5f6159956fc8b35b3433c2f5f36
obolenski <60326681+obolenski@users.noreply.github.com>
2024-06-24 20:29:46 +0200

    add aws myApllications tag

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -3,5 +3,10 @@ locals {
     "repository"             = "https://github.com/obolenski/obsidian-secretary"
     "created_with_terraform" = "true"
     "application"            = "obsidian-secretary"
+    "awsApplication"         = "arn:aws:resource-groups:eu-central-1:${var.account_number}:group/obsidian-secretary/02qnti2memrf2c6xtop2dmtmsj"
   }
+}
+
+variable "account_number" {
+  type = string
 }
