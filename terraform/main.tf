97ba4e1e0b5fa6830b492d4219fb8058942b6958
obolenski <60326681+obolenski@users.noreply.github.com>
2024-07-30 23:04:58 +0200

    add backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,12 @@
 provider "aws" {
+  region = "eu-central-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "obolenski-terraform-state"
+    key    = "obsidian-secretary.tfstate"
     region = "eu-central-1"
+  }
 }
 
